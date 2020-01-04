8054a5083f1a1dd13bb2ed94ef9eb58539243579
igor <perrofeofilms@gmail.com>
2020-01-03 19:34:26 -0600

    -

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,18 +1,3 @@
-<script>
-  import { onMount } from 'svelte';
-
-  onMount(() => {
-    if (window.netlifyIdentity) {
-      window.netlifyIdentity.on("init", user => {
-        if (!user) {
-          window.netlifyIdentity.on("login", () => {
-            document.location.href = "/admin/";
-          });
-        }
-      });
-    }
-  });
-</script>
 <style>
 	h1, figure, p {
 		text-align: center;
